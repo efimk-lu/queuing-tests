f5b1f609121c08087c23e46aba570b426dafbcf7
Efi Merdler-Kravitz <efi@lumigo.io>
2018-11-28 17:07:38 +0200

    Add kinesis

--- src/lambdas/kinesis-handler.js
+++ src/lambdas/kinesis-handler.js
@@ -4,13 +4,12 @@ const sleep = promisify(setTimeout);
 
 module.exports.handler = async (event, context) => {
   event.Records.forEach(record => {
-    const { body, attributes } = record;
-    let message = JSON.parse(body);
+    const { kinesis } = record;
+    let message = JSON.parse(kinesis);
     console.info("Message received");
-    console.info(
-      `inLatency=${attributes.SentTimestamp -
-        message.sentTime}, outLatency=${Date.now() - attributes.SentTimestamp},`
-    );
+    console.info(message);
+    let data = Buffer.from(message.data, "base64"); // Ta-da
+    console.info(`latency=${Date.now() - data.sentTime},`);
   });
   await sleep(1500);
   return { code: 200 };
