aac6aa149d4b0c4c0143afa17955338be71cbdd5
Efi Merdler-Kravitz <efi@lumigo.io>
2018-11-28 19:16:05 +0200

    Added more details to kinesis lambda

--- src/lambdas/kinesis-handler.js
+++ src/lambdas/kinesis-handler.js
@@ -7,9 +7,12 @@ module.exports.handler = async (event, context) => {
     const { kinesis } = record;
     let message = JSON.parse(kinesis);
     console.info("Message received");
-    console.info(message);
-    let data = Buffer.from(message.data, "base64"); // Ta-da
-    console.info(`latency=${Date.now() - data.sentTime},`);
+    let data = Buffer.from(message.data, "base64");
+    const arrivedToKinesis = message.approximateArrivalTimestamp * 1000;
+    console.info(
+      `inLatency=${arrivedToKinesis - data.sentTime}, outLatency=${Date.now() -
+        arrivedToKinesis},`
+    );
   });
   await sleep(1500);
   return { code: 200 };
