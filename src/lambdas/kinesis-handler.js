4d50d761d872f3599057265a16d2e0e5542d9aba
Efi Merdler-Kravitz <efi@lumigo.io>
2018-11-29 10:32:12 +0200

    Improve kinesis handler

--- src/lambdas/kinesis-handler.js
+++ src/lambdas/kinesis-handler.js
@@ -5,10 +5,9 @@ const sleep = promisify(setTimeout);
 module.exports.handler = async (event, context) => {
   event.Records.forEach(record => {
     const { kinesis } = record;
-    let message = JSON.parse(kinesis);
     console.info("Message received");
-    let data = Buffer.from(message.data, "base64");
-    const arrivedToKinesis = message.approximateArrivalTimestamp * 1000;
+    let data = JSON.parse(Buffer.from(kinesis.data, "base64"));
+    const arrivedToKinesis = kinesis.approximateArrivalTimestamp * 1000;
     console.info(
       `inLatency=${arrivedToKinesis - data.sentTime}, outLatency=${Date.now() -
         arrivedToKinesis},`
